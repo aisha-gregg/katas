4db224c0deed272c435bf00ddc4c535dfbfe7c04
Aisha Gregg <aishagregg@gmail.com>
2020-06-06 23:18:43 +0200

    clock solution test 2

--- clock/solutions/clock-aisha.spec.ts
+++ clock/solutions/clock-aisha.spec.ts
@@ -7,4 +7,11 @@ describe('function clock', () => {
     const actual = clock(given)
     expect(actual).toBe('00:00:30')
   })
+
+  it('should take a time in seconds that are more than 60 and return it in the following format hh:mm:ss', () => {
+    const given = 70
+
+    const actual = clock(given)
+    expect(actual).toBe('00:01:10')
+  })
 })
--- clock/solutions/clock-aisha.ts
+++ clock/solutions/clock-aisha.ts
@@ -1,5 +1,9 @@
 export function clock(seconds: number): string {
-  const formatSeconds = `00:00:${seconds}`
-
+  let formatSeconds = `00:00:${seconds}`
+  if (seconds > 60) {
+    const calculateMinutes = Math.floor(seconds / 60)
+    const calculateSeconds = seconds % 60
+    formatSeconds = `00:0${calculateMinutes}:${calculateSeconds}`
+  }
   return formatSeconds
 }
