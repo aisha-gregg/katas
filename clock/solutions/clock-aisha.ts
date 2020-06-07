e2ce4d9f8a689cf7f82e92641413459a3f0f10ef
Aisha Gregg <aishagregg@gmail.com>
2020-06-07 16:04:31 +0200

    Refactor code

--- clock/solutions/clock-aisha.ts
+++ clock/solutions/clock-aisha.ts
@@ -1,24 +1,10 @@
 export function clock(seconds: number): string {
   const secondsInAnHour = 3_600
   const secondsInAMinute = 60
-  let newSeconds = 0
-  let newMinutes = 0
-  let newHours = 0
-  if (seconds >= secondsInAnHour) {
-    newHours = Math.floor(seconds / secondsInAnHour)
-    seconds = seconds % secondsInAnHour
-  }
-
-  if (seconds >= secondsInAMinute) {
-    newMinutes = Math.floor(seconds / secondsInAMinute)
-    seconds = seconds % secondsInAMinute
-  }
-
-  if (seconds < secondsInAMinute) {
-    newSeconds = seconds
-  }
-
+  const hours = Math.floor(seconds / secondsInAnHour)
+  seconds %= secondsInAnHour
+  const minutes = Math.floor(seconds / secondsInAMinute)
+  seconds %= secondsInAMinute
   const padLeft = (time: number) => (time < 10 ? '0' + time : time)
-
-  return `${padLeft(newHours)}:${padLeft(newMinutes)}:${padLeft(newSeconds)}`
+  return padLeft(hours) + ':' + padLeft(minutes) + ':' + padLeft(seconds)
 }
