f7b1dfcb089954fb25c76878abea5a54e6ab1edd
Aisha Gregg <aishagregg@gmail.com>
2020-06-07 13:48:38 +0200

    test  over 1 hour completed

--- clock/solutions/clock-aisha.spec.ts
+++ clock/solutions/clock-aisha.spec.ts
@@ -8,10 +8,38 @@ describe('function clock', () => {
     expect(actual).toBe('00:00:30')
   })
 
-  it('should take a time in seconds that are more than 60 and return it in the following format hh:mm:ss', () => {
+  it('should take a time in seconds that is more than 60 and return it in the following format hh:mm:ss', () => {
     const given = 70
 
     const actual = clock(given)
     expect(actual).toBe('00:01:10')
+  })
+
+  it('should take a time in seconds that is 10 minutes and return it in the following format hh:mm:ss', () => {
+    const given = 600
+
+    const actual = clock(given)
+    expect(actual).toBe('00:10:00')
+  })
+
+  it('should take a time in seconds that is 1 hour and return it in the following format hh:mm:ss', () => {
+    const given = 3600
+
+    const actual = clock(given)
+    expect(actual).toBe('01:00:00')
+  })
+
+  it('should take a time in seconds that is over 1 hour and return it in the following format hh:mm:ss', () => {
+    const given = 3660
+
+    const actual = clock(given)
+    expect(actual).toBe('01:01:00')
+  })
+
+  it('should take a time in seconds that is over 1 hour and 10 mintues and return it in the following format hh:mm:ss', () => {
+    const given = 4260
+
+    const actual = clock(given)
+    expect(actual).toBe('01:01:00')
   })
 })
--- clock/solutions/clock-aisha.ts
+++ clock/solutions/clock-aisha.ts
@@ -1,9 +1,35 @@
 export function clock(seconds: number): string {
   let formatSeconds = `00:00:${seconds}`
-  if (seconds > 60) {
+  if (seconds > 60 && seconds < 600) {
     const calculateMinutes = Math.floor(seconds / 60)
     const calculateSeconds = seconds % 60
     formatSeconds = `00:0${calculateMinutes}:${calculateSeconds}`
+  }
+  if (seconds >= 600) {
+    const calculateMinutes = Math.floor(seconds / 60)
+    const calculateSeconds = seconds % 60
+    formatSeconds = `00:${calculateMinutes}:${calculateSeconds}0`
+  }
+  if (seconds === 3600) {
+    const calculateMinutes = Math.floor(seconds / 60)
+    if (calculateMinutes >= 60) {
+      const calculateHours = Math.floor(calculateMinutes / 60)
+
+      const calculateSeconds = seconds % 60
+
+      formatSeconds = `0${calculateHours}:00:${calculateSeconds}0`
+    }
+  }
+
+  if (seconds > 3600) {
+    let calculateMinutes = Math.floor(seconds / 60)
+    if (calculateMinutes >= 60) {
+      const calculateHours = Math.floor(calculateMinutes / 60)
+      calculateMinutes = calculateHours % 60
+      const calculateSeconds = seconds % 60
+
+      formatSeconds = `0${calculateHours}:0${calculateMinutes}:${calculateSeconds}0`
+    }
   }
   return formatSeconds
 }
